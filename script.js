8180b01a3cf7ba134fc9f58c1c9fd8c42b450ccc
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-23 17:17:01 +0530

    first

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var Engine = Matter.Engine
 var World = Matter.World
 var Bodies = Matter.Bodies;
 
+var Constraint = Matter.Constraint;
+
 
 var engine;
 var world;
@@ -23,7 +25,28 @@ function setup() {
     // boundaries.push(new Boundary(150, 100, width * 0.3, 20, 0.3));
     // boundaries.push(new Boundary(250, 400, width * 0.6, 20, -0.3));
 
-    boundaries.push(new Boundary(200, height, width, 50, 0));
+    for (var x = 20; x < 380; x+= 40) {
+        var p = new Particle(x, 100, 10);
+        // var p1 = new Particle(200, 150, 10);
+        particles.push(p);
+
+     // particles.push(p1);
+
+        //     var options = {
+        //         bodyA: p.body,
+        //         bodyB: p1.body,
+        //         length: 50,
+        //         stiffnes: 0.4
+        //     }
+
+        //   var constraint = Constraint.create(options);
+
+        //   World.add(world, constraint);
+    }
+    
+   
+
+    boundaries.push(new Boundary(200, height, width, 30, 0));
     
 
     
@@ -45,6 +68,12 @@ function draw() {
     for(var i = 0; i < boundaries.length; i++) {
         boundaries[i].show();
     }
+
+    for(var i = 0; i < particles.length; i++) {
+        particles[i].show();
+    }
+
+    // line(particles[0].body.position.x, particles[0].body.position.y, particles[1].body.position.x, particles[1].body.position.y);
 
 
     
